6894eb88a7d26a7038e223c3272e22c8e451145a
antonio <mr.antonsilin@gmail.com>
2024-04-12 10:44:27 +0200

    container list ready

--- docker_inspector/app.py
+++ docker_inspector/app.py
@@ -1,10 +1,16 @@
+import subprocess
+from rich.text import Text
+import json
+import os
+
 from textual.app import App, ComposeResult
 from textual.screen import Screen
-from textual.containers import ScrollableContainer
-from textual.widgets import Header, Footer, Button, Static, DataTable, Log, Label, TabbedContent, TabPane, Placeholder
+from textual.containers import Horizontal
+from textual.widgets import Header, Footer, Button, DataTable, Log, Label, TabbedContent, TabPane, Placeholder, Select
 from textual.reactive import reactive
 
 from .widgets.container_list import ContainerList
+from .utils.get_info import get_info
 
 
 # class TestScreen(Screen):
@@ -22,10 +28,23 @@ class DockerInspectorApp(App):
         ("d", "toggle_dark", "Toggle dark mode"),
     ]
 
+    projects = reactive([])
+    containers = reactive([])
+    volumes = reactive([])
+    filter_project = reactive(None)
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
+        yield Horizontal(
+            Button("Refresh", id="refresh", variant="primary"),
+            Select(
+                [],
+                prompt='All projects', id='select_project'
+            ),
+            id="top-menu"
+        )
         with TabbedContent():
             with TabPane("Containers"):
                 yield ContainerList()
@@ -33,13 +52,31 @@ class DockerInspectorApp(App):
                 yield Placeholder("TODO: Volumes", id="volumes")
 
     def action_refresh(self) -> None:
-        """An action to refresh the container list."""
+        self.projects, self.containers = get_info()
+
+        select = self.query_one('#select_project', Select)
+        select.set_options([(p, p) for p in self.projects])
+
         container_list = self.query_one(ContainerList)
-        container_list.refresh_data()
+        container_list.refresh_data(self.containers, self.filter_project)
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
+
+    def on_mount(self) -> None:
+        self.action_refresh()
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "refresh":
+            self.action_refresh()
+
+    def on_select_changed(self, event: Select.Changed) -> None:
+        if event.select.id == "select_project":
+            self.filter_project = event.select.value if event.select.value != Select.BLANK else None
+
+            container_list = self.query_one(ContainerList)
+            container_list.refresh_data(self.containers, self.filter_project)
 
 
 def run():
